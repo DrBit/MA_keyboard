b393dd9ef1527a87a9fb7fa00460bf76508fc383
DrBit <betamaster50@gmail.com>
2013-10-02 19:58:04 +0200

    V0.1 First working version based on examples, keys + artnet

--- MA_Keyboard.ino
+++ MA_Keyboard.ino
@@ -40,6 +40,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println("MA Keyboard Test");
   init_DBs ();
+  setup_artnet ();
   Show_all_records();
   Serial.println("Press ` Key to access mapping functions");
 }
@@ -61,33 +62,10 @@ void loop() {
 		// Execute function attached to S
 		Send_DMX_Function (record_value);
 	}
-    /*
-    // check for some of the special keys
-    if (c == PS2_ENTER) {
-      Serial.println();
-    } else if (c == PS2_TAB) {
-      Serial.print("[Tab]");
-    } else if (c == PS2_ESC) {
-      Serial.print("[ESC]");
-    } else if (c == PS2_PAGEDOWN) {
-      Serial.print("[PgDn]");
-    } else if (c == PS2_PAGEUP) {
-      Serial.print("[PgUp]");
-    } else if (c == PS2_LEFTARROW) {
-      Serial.print("[Left]");
-    } else if (c == PS2_RIGHTARROW) {
-      Serial.print("[Right]");
-    } else if (c == PS2_UPARROW) {
-      Serial.print("[Up]");
-    } else if (c == PS2_DOWNARROW) {
-      Serial.print("[Down]");
-    } else if (c == PS2_DELETE) {
-      Serial.print("[Del]");
-    } else {
-      // otherwise, just print all normal characters
-      Serial.print(c);
-    }*/
   }
+
+  // Artnet handling
+  loop_artnet();
 }
 
 void mapping_keys () {
